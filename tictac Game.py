802fd52c5d30a75a32bd12d0f51e1b1dbdd5d7de
evance ngowi <ngowievance40@gmail.com>
2023-12-18 13:34:31 +0300

    Add more button

--- tictac Game.py
+++ tictac Game.py
@@ -1,8 +1,10 @@
 from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
-root =Tk()
+root = tk.Tk()
 root.geometry('560x350')
+root.title=("Tic Tac Toe Game")
 #ROW ONE
 button1=ttk.Button(root,text="",command=lambda:buttonPressed(1))
 button1.grid(row=0,column=0,ipadx=50,ipady=50)
@@ -22,6 +24,17 @@ button5.grid(row=2,column=1,ipadx=50,ipady=50)
 
 button6=ttk.Button(root,text="",command=lambda:buttonPressed(6))
 button6.grid(row=2,column=3,ipadx=50,ipady=50)
+
+#ROW THREE
+button7=ttk.Button(root,text="",command=lambda:buttonPressed(7))
+button7.grid(row=3,column=0,ipadx=50,ipady=50)
+
+button8 =ttk.Button(root,text="",command=lambda:buttonPressed(8))
+button8.grid(row=3,column=1,ipadx=50,ipady=50)
+
+button9=ttk.Button(root,text="",command=lambda:buttonPressed(9))
+button9.grid(row=3,column=3,ipady=50,ipadx=50)
+
 def buttonPressed(buttonNumber):
     temp=buttonNumber
 
