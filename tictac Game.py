879c1b36d56ae1b587f516b67032077244087e11
evance ngowi <ngowievance40@gmail.com>
2023-12-19 18:13:50 +0300

    Create condition for each button

--- tictac Game.py
+++ tictac Game.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
-root.geometry('560x350')
+root.geometry('760x450')
 root.title=("Tic Tac Toe Game")
 #ROW ONE
 button1=ttk.Button(root,text="",command=lambda:buttonPressed(1))
@@ -35,8 +35,57 @@ button8.grid(row=3,column=1,ipadx=50,ipady=50)
 button9=ttk.Button(root,text="",command=lambda:buttonPressed(9))
 button9.grid(row=3,column=3,ipady=50,ipadx=50)
 
+player =1
 def buttonPressed(buttonNumber):
-    temp=buttonNumber
+    global player
+    if buttonNumber ==1 and player==1:
+        button1.config(text="X")
+        player =2
+    elif buttonNumber ==1 and player ==2:
+        button1.config(text="O")
+        player=1
 
+    elif buttonNumber ==2 and player ==1:
+        button2.config(text="X")
+        player =2
+    elif buttonNumber ==2 and player==2:
+        button2.config(text="O")
+        player =1
+    
+    elif buttonNumber==3 and player==1:
+        button3.config(text="X")
+        player =2
+    elif buttonNumber==3 and player==2:
+        button3.config(text="O")
+        player =1
 
+    elif buttonNumber==4 and player==1:
+        button4.config(text="X")
+        player =2
+    elif buttonNumber==4 and player==2:
+        button4.config(text="O")
+
+    elif buttonNumber==5 and player==1:
+        button5.config(text="X")
+    elif buttonNumber==5 and player==2:
+        button5.config(text="O")
+
+    elif buttonNumber ==6 and player==1:
+        button6.config(text="X")
+    elif buttonNumber==6 and player==2:
+        button6.config(text="O")
+
+    elif buttonNumber ==7 and player==1:
+        button7.config(text="X")
+    elif buttonNumber ==7 and player==2:
+        button7.config(text="O")
+
+    elif buttonNumber ==8 and player==1:
+        button8.config(text="X")
+    elif buttonNumber ==8 and player ==2:
+        button8.config(text="X")
+    elif buttonNumber ==9 and player ==1:
+        button9.config(text="X")
+    elif buttonNumber ==9 and player==2:
+        button9.config(text="0")
 root.mainloop()
