fddef6e6361226b081e1ab8262693bcdeaf8dffa
evance ngowi <ngowievance40@gmail.com>
2023-12-19 19:44:13 +0300

    Remove deleted files

--- tictac Game.py
+++ tictac Game.py
@@ -1,7 +1,30 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
+def checkwinner():
+    if (button1['text']=='0' and button2['text']=='0' and button3['text']=='0' or
+        button4['text'] =='0' and button5['text']=='0' and button6['text']=='0' or
+        button7['text'] =='0' and button8['text']=='0'and button8['text']=='0' or
+        button1['text']=='0' and button4['text']=='0' and button7['text']=='0' or
+        button2['text']=='0' and button5['text']=='0'and button8['text']=='0'or
+        button3['text']=='0' and button6['text']=='0'and button9['text']=='0'or
+        button1['text']=='0' and button5['text']=='0' and button9['text']=='0'or
+        button3['text']=='0'and button5['text']=='0' and button7['text']=='0'):
+           messagebox._show("winner of the game",'Player O is winner')
+           
+
+    elif (button1['text']=='X' and button2['text']=='X' and button3['text']=='X' or
+         button4['text'] =='X' and button5['text']=='X' and button6['text']=='X' or
+         button7['text'] =='X' and button8['text']=='X'and button8['text']=='X' or
+         button1['text']=='X' and button4['text']=='X' and button7['text']=='X' or
+         button2['text']=='X' and button5['text']=='X'and button8['text']=='X'or
+         button3['text']=='X' and button6['text']=='X'and button9['text']=='X'or
+         button1['text']=='X' and button5['text']=='X' and button9['text']=='X'or
+         button3['text']=='X'and button5['text']=='X' and button7['text']=='X'):
+             messagebox._show("Winner of the game","Player X is winner")
+    
 root = tk.Tk()
 root.geometry('760x450')
 root.title=("Tic Tac Toe Game")
@@ -42,50 +65,59 @@ def buttonPressed(buttonNumber):
         button1.config(text="X")
         player =2
     elif buttonNumber ==1 and player ==2:
-        button1.config(text="O")
+        button1.config(text="0")
         player=1
 
     elif buttonNumber ==2 and player ==1:
         button2.config(text="X")
         player =2
     elif buttonNumber ==2 and player==2:
-        button2.config(text="O")
+        button2.config(text="0")
         player =1
     
     elif buttonNumber==3 and player==1:
         button3.config(text="X")
         player =2
     elif buttonNumber==3 and player==2:
-        button3.config(text="O")
+        button3.config(text="0")
         player =1
 
     elif buttonNumber==4 and player==1:
         button4.config(text="X")
         player =2
     elif buttonNumber==4 and player==2:
-        button4.config(text="O")
-
+        button4.config(text="0")
+        player=1
     elif buttonNumber==5 and player==1:
         button5.config(text="X")
+        player=2
     elif buttonNumber==5 and player==2:
-        button5.config(text="O")
-
+        button5.config(text="0")
+        player=1
     elif buttonNumber ==6 and player==1:
         button6.config(text="X")
+        player=2
     elif buttonNumber==6 and player==2:
-        button6.config(text="O")
-
+        button6.config(text="0")
+        player=1
     elif buttonNumber ==7 and player==1:
         button7.config(text="X")
+        player=2
     elif buttonNumber ==7 and player==2:
-        button7.config(text="O")
-
+        button7.config(text="0")
+        player=1
     elif buttonNumber ==8 and player==1:
         button8.config(text="X")
+        player=2
     elif buttonNumber ==8 and player ==2:
-        button8.config(text="X")
+        button8.config(text="0")
+        player=1
     elif buttonNumber ==9 and player ==1:
         button9.config(text="X")
+        player=2
     elif buttonNumber ==9 and player==2:
         button9.config(text="0")
+        player=1
+
+    checkwinner()
 root.mainloop()
