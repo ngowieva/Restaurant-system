f4900a1ca178f274bba9e8ceaeb96e03ff0ec426
evance ngowi <ngowievance40@gmail.com>
2023-12-20 20:46:09 +0300

    import some library

--- detect questions.py
+++ detect questions.py
@@ -1,8 +1,13 @@
+from nltk.tokenize import word_tokenize # nltk(natural language toolkit)
+from nltk.tokenize import word_tokenize
+import nltk
+nltk.download('punkt') #installing this specific resource so that NLTK can use it for tokenization tasks.
+
 statiment_words =['what','where','who','when','name','is','do','does','which','are','could','would'
                   ,'should','has','whom','whose',"don't"]
 sentence=input("Enter a Sentence :\n")
 sentence=sentence.lower()
-
+sentence =word_tokenize(sentence)
 if any(x in sentence[0]for x in statiment_words):
     print("This is the question")
 else:
