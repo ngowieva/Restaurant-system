84f862775d616e479ddded208db5351870e488fc
evance ngowi <ngowievance40@gmail.com>
2023-12-29 19:56:53 +0300

    add comment

--- detect questions.py
+++ detect questions.py
@@ -4,7 +4,7 @@ import nltk
 nltk.download('punkt') #installing this specific resource so that NLTK can use it for tokenization tasks.
 
 statiment_words =['what','where','who','when','name','is','do','does','which','are','could','would'
-                  ,'should','has','whom','whose',"don't"]
+                  ,'should','has','whom','whose',"don't","why"]
 print("THIS IS THE PROGRAM TO TEST IF SENTENCE IS QUESTION OR NOT")
 sentence=input("Enter English Sentence Only:\n")
 sentence=sentence.lower()
@@ -13,3 +13,4 @@ if any(x in sentence[0]for x in statiment_words):
     print("This is the question")
 else:
     print("This is not a question")
+    exit()
